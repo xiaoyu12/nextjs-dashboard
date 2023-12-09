f1ebb0d8a41e0fbb01ea85efaeaf1cfca6dca5cd
Xiaoyu Zhang <xiaoyu@csusm.edu>
2023-12-08 17:13:45 -0800

    following tutorials

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+      },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://localhost:3000'),
+};
 
 export default function RootLayout({
   children,
